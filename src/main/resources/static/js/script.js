6beff4d0281921399447e90612701778da7809d9
Leandro Lopez <leonightwish@gmail.com>
2021-04-01 19:33:58 -0300

    Atualização

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,125 @@
+
+$(document).ready(function () {
+	$.ajax({
+		  type: "get",
+		  url: "/dominio/estadocivil",
+		  data: { tipos: $("#estadoCivil").val() },
+		  dataType: 'json',
+		  contentType: "application/json; charset=utf-8",
+		  success: function (data) {
+		          var selectbox = $('#estadoCivil');
+		          $.each(data, function (i, d) {
+		              selectbox.append('<option value="' + d.idDominio+ '">' + d.valor + '</option>');
+		          });
+		  } });
+});
+
+
+$(document).ready(function () {
+	$.ajax({
+		  type: "get",
+		  url: "/dominio/genero",
+		  data: { tipos: $("#genero").val() },
+		  dataType: 'json',
+		  contentType: "application/json; charset=utf-8",
+		  success: function (data) {
+		          var selectbox = $('#genero');
+		          $.each(data, function (i, d) {
+		              selectbox.append('<option value="' + d.idDominio+ '">' + d.valor + '</option>');
+		          });
+		  } });
+});
+
+//Janela Modal
+$(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+  		
+        var id = $(this).attr("href");
+  
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+      
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+  
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+      
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+        
+       
+    });
+  
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+  
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
+
+
+//Carrega Tabela
+$(document).on("click", "#btn", function () {
+		$.ajax ({
+			type: 'GET',
+			url: '/ranking/fromuserdataArq',
+			dataType: 'json',
+			contentType: 'application/json',
+			success: function(result){		
+				 var table = '';
+				 table += '<tr><th>ID</th><th>Id</th><th>Desc.Produto</th><th>Desc.Plano</th><th>Nm.Ramo</th><th>Rating</th></tr>';
+				  var x = 0;
+				  while (x < result.length) {
+				    table += '<tr><td id="id_produto">' + result[x].id_produto + '</td>';
+				    table += '<td id="id_plano">' + result[x].id_plano + '</td>';
+				    table += '<td id="descProduto">' + result[x].descProduto + '</td>';
+				    table += '<td id="descPlano">' + result[x].descPlano + '</td>';
+				    table += '<td id="nm_ramo">' + result[x].nm_ramo + '</td>';
+				    table += '<td id="rating">' + result[x].rating + '</td></tr>';
+				    x++;
+				  }
+				  $('#table tbody').html(table);
+			}
+		});
+	});
+	
+//Carrega Dados Form
+$(document).on("click", "#btPesquisar", function () {
+
+	var civil = $("#estadoCivil").val();
+	var genero = $("#genero").val();
+	var idade = $("#idade").val();
+	var peso = $("#peso").val();
+	var table = '';
+			
+	$.ajax ({
+		type: 'GET',
+		url: '/ranking/fromuserdataArq2?id_modelo=1&genero='+genero+'&estadocivil='+civil+'&peso='+peso+'&idade='+idade+'',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function(result){	
+			
+			 var y = 0;
+			 table += '<tr><th>Gênero</th><th>Estado Civil</th><th>Peso</th><th>Idade</th></tr>';
+			  while (y < result.length) {
+			    table += '<tr><td id="genero">' + result[y].genero + '</td>';
+			    table += '<td id="estadoCivil">' + result[y].estadoCivil + '</td>';
+			    table += '<td id="peso">' + result[y].peso + '</td>';
+			    table += '<td id="idade">' + result[y].idade + '</td></tr>';
+			    y++;
+			  }
+
+			$('#Modaltable tbody').html(table);
+		}
+	});
+});	
+
